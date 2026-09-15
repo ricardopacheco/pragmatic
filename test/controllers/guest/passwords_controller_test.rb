b18b35f0c849bbcbe2b2ed415fc3b31706991eee
Ricardo Pacheco <innervisiondev@gmail.com>
2026-09-15 11:25:03 -0300

    chore: test doubles, factories and development conveniences (#19)

--- test/controllers/guest/passwords_controller_test.rb
+++ test/controllers/guest/passwords_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 module Guest
   class PasswordsControllerTest < ActionDispatch::IntegrationTest
-    setup { @user = User.take }
+    setup { @user = create(:user, password: "password") }
 
     test "new" do
       get new_password_path
--- test/controllers/guest/sessions_controller_test.rb
+++ test/controllers/guest/sessions_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 module Guest
   class SessionsControllerTest < ActionDispatch::IntegrationTest
-    setup { @user = User.take }
+    setup { @user = create(:user, password: "password") }
 
     test "new" do
       get new_session_path
