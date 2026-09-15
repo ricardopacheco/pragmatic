cf9a75d90f7a81453e36a809003d70895d9faa6f
Ricardo Pacheco <innervisiondev@gmail.com>
2026-09-15 11:37:17 -0300

    feat: i18n foundation with en and pt (#21)

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,9 @@ module Pragmatic
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.available_locales = %i[en pt]
+    config.i18n.default_locale = :en
+    config.i18n.load_path += Rails.root.glob("config/locales/**/*.yml")
   end
 end
